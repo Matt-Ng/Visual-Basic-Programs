4535972d973df4ef01030ba242e35d3c4e55389b
Matthew Ng <the.matthew.ng@gmail.com>
2017-05-02 22:13:03 -0400

    Add files via upload

---
+++ Triangle SAS/Triangle SAS/Form1.Designer.vb
@@ -0,0 +1,163 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Answer = New System.Windows.Forms.TextBox()
+        Me.Calculate = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(7, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(315, 16)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Triangle Calculator Multiple Solution Super Program"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(35, 108)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 13)
+        Me.Label2.TabIndex = 1
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(35, 154)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(0, 13)
+        Me.Label3.TabIndex = 2
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(35, 197)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(0, 13)
+        Me.Label4.TabIndex = 3
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(39, 267)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(42, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Answer"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Base x Height", "Heron's Formula", "Side Angle Side", "Hypotenuse", "Degree to Radian"})
+        Me.ComboBox1.Location = New System.Drawing.Point(87, 55)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 5
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(87, 108)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(121, 20)
+        Me.TextBox1.TabIndex = 6
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(87, 154)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(121, 20)
+        Me.TextBox2.TabIndex = 7
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(87, 197)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(121, 20)
+        Me.TextBox3.TabIndex = 8
+        '
+        'Answer
+        '
+        Me.Answer.Location = New System.Drawing.Point(88, 259)
+        Me.Answer.Name = "Answer"
+        Me.Answer.ReadOnly = True
+        Me.Answer.Size = New System.Drawing.Size(100, 20)
+        Me.Answer.TabIndex = 9
+        '
+        'Calculate
+        '
+        Me.Calculate.Location = New System.Drawing.Point(99, 230)
+        Me.Calculate.Name = "Calculate"
+        Me.Calculate.Size = New System.Drawing.Size(75, 23)
+        Me.Calculate.TabIndex = 10
+        Me.Calculate.Text = "Calculate!"
+        Me.Calculate.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(334, 284)
+        Me.Controls.Add(Me.Calculate)
+        Me.Controls.Add(Me.Answer)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "i"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Answer As System.Windows.Forms.TextBox
+    Friend WithEvents Calculate As System.Windows.Forms.Button
+
+End Class
---
+++ Triangle SAS/Triangle SAS/Form1.vb
@@ -0,0 +1,89 @@
+﻿Public Class Form1
+    Dim a, b, c, ans As Double
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        ' if base x height is selected, labels fill in
+        If ComboBox1.Text = "Base x Height" Then
+            Label2.Text = "Base"
+            Label3.Text = "Height"
+            Label4.Text = " "
+            ' if herons formula is selected, labels fill in
+        ElseIf ComboBox1.Text = "Heron's Formula" Then
+            Label2.Text = "a"
+            Label3.Text = "b"
+            Label4.Text = "c"
+            ' if sas is selected, labels fill in
+        ElseIf ComboBox1.Text = "Side Angle Side" Then
+            Label2.Text = "Side 1"
+            Label3.Text = "Side 2"
+            Label4.Text = "Angle"
+            ' if hypotenuse is selected, labels fill in
+        ElseIf ComboBox1.Text = "Hypotenuse" Then
+            Label2.Text = "Side A"
+            Label3.Text = "Side B"
+
+            ' if degree to radian is selected, labels fill in
+        ElseIf ComboBox1.Text = "Degree to Radian" Then
+            Label2.Text = "Angle"
+            Label3.Text = " "
+            Label4.Text = " "
+        End If
+        
+
+
+    End Sub
+
+    Private Sub Calculate_Click(sender As Object, e As EventArgs) Handles Calculate.Click
+        ' base x height
+        If ComboBox1.Text = "Base x Height" Then
+            'declaring variables
+            a = TextBox1.Text
+            b = TextBox2.Text
+            'formula for base x height
+            ans = Math.Round(a * b / 2, 2)
+            'output to textbox
+            Answer.Text = ans
+            ' herons forumla
+        ElseIf ComboBox1.Text = "Heron's Formula" Then
+            'declaring variables
+            a = TextBox1.Text
+            b = TextBox2.Text
+            c = TextBox3.Text
+            'variable for s in heron's formula
+            Dim s As Double
+            'formula to find s
+            s = ((a + b + c) / 2)
+            'formula to find the area
+            ans = Math.Round(Math.Sqrt(s * (s - a) * (s - b) * (s - c)), 2)
+            'output to textbox
+            Answer.Text = ans
+        ElseIf ComboBox1.Text = "Side Angle Side" Then
+            'declaring variables
+            a = TextBox1.Text
+            b = TextBox2.Text
+            c = TextBox3.Text
+            Dim sine As Double
+            'finding sin c
+            sine = Math.Sin(c)
+            'formula for sas
+            ans = Math.Round((a * b * sine) / 2, 2)
+            'output to textbox
+            Answer.Text = ans
+        ElseIf ComboBox1.Text = "Hypotenuse" Then
+            'declaring variables
+            a = TextBox1.Text
+            b = TextBox2.Text
+            'Hypotenuse formula
+            ans = Math.Sqrt(a * a + b * b)
+            'output to textbox
+            Answer.Text = Math.Round(ans, 2)
+        ElseIf ComboBox1.Text = "Degree to Radian" Then
+            'declaring variable
+            a = TextBox1.Text
+            'formula for degree to radian
+            ans = Math.Round((a * Math.PI) / 2, 2)
+            'output to textbox
+            Answer.Text = ans
+        End If
+    End Sub
+End Class
